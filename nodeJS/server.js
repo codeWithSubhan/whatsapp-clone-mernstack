c41a53ac0145970417c9c8fa62c1ab2cc71ee349
suvhan <suvhan13@gmail.com>
2024-12-11 10:57:32 +0530

    final build

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -37,7 +37,8 @@ server.listen(port, () =>
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "whatsapp-clone-mernstack-q7li.vercel.app",
   },
 });
 
